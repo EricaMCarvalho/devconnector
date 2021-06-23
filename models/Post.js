e4b6b68baaa94cad6b7b6a1e8ef7172ff9390882
Erica Carvalho <ericamcar28@gmail.com>
2021-06-23 14:53:26 +0100

    finish post model

--- models/Post.js
+++ models/Post.js
@@ -46,6 +46,28 @@ const PostSchema = new Schema({
           },
         },
       ],
+      comments: [
+        {
+          user: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+          },
+          text: {
+            type: String,
+            required: true,
+          },
+          name: {
+            type: String,
+          },
+          avatar: {
+            type: String,
+          },
+          date: {
+            type: Date,
+            default: Date.now,
+          },
+        },
+      ],
       date: {
         type: Date,
         default: Date.now,
