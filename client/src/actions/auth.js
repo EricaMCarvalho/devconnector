8cfacc477915652a5e0026a7731da15dc2fa169c
Erica Carvalho <ericamcar28@gmail.com>
2021-06-24 12:05:01 +0100

    logout and navbar links

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,6 +6,7 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
+  LOGOUT,
 } from './types';
 import { setAlert } from './alert';
 import setAuthToken from '../utils/setAuthToken';
@@ -95,4 +96,9 @@ export const login = (email, password) => async (dispatch) => {
       type: LOGIN_FAIL,
     });
   }
+};
+
+// Logout / Clear profile
+export const logout = () => async (dispatch) => {
+  dispatch({ type: LOGOUT });
 };
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -5,6 +5,7 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
+  LOGOUT,
 } from '../actions/types';
 
 const initialState = {
@@ -35,6 +36,7 @@ function auth(state = initialState, action) {
     case REGISTER_FAIL:
     case LOGIN_FAIL:
     case AUTH_ERROR:
+    case LOGOUT:
       localStorage.removeItem('token');
       return {
         ...state,
